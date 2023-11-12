ccddda4f12a83c51c3ddc3ca9e544992c8a64873
Shashank Joshi <shashankjoshi368@gmail.com>
2023-11-12 15:34:53 -0500

    new changes added

--- courses/routes.js
+++ courses/routes.js
@@ -1,8 +1,9 @@
 import Database from "../Database/index.js";
+let dbCourses = Database.courses;
 function CourseRoutes(app) {
   app.get("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    const course = Database.courses.find((c) => c._id === id);
+    const course = dbCourses.find((c) => c._id === id);
     if (!course) {
       res.status(404).send("Course not found");
       return;
@@ -11,28 +12,26 @@ function CourseRoutes(app) {
   });
 
   app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
+    const courses = dbCourses;
     res.send(courses);
   });
 
   app.post("/api/courses", (req, res) => {
     const course = { ...req.body, _id: new Date().getTime().toString() };
-    Database.courses.push(course);
+    dbCourses.push(course);
     res.send(course);
   });
 
   app.delete("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    Database.courses = Database.courses.filter((c) => c._id !== id);
+    dbCourses = dbCourses.filter((c) => c._id !== id);
     res.sendStatus(204);
   });
 
   app.put("/api/courses/:id", (req, res) => {
     const { id } = req.params;
     const course = req.body;
-    Database.courses = Database.courses.map((c) =>
-      c._id === id ? { c, ...course } : c
-    );
+    dbCourses = dbCourses.map((c) => (c._id === id ? { ...c, ...course } : c));
     res.sendStatus(204);
   });
 }
