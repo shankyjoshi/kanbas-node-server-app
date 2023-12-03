dee711501f1a16f31495d0ad20f2dbd0a8a79d1f
Shashank Joshi <shashankjoshi368@gmail.com>
2023-12-02 19:30:40 -0500

    commit

--- users/routes.js
+++ users/routes.js
@@ -31,8 +31,7 @@ function UserRoutes(app) {
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
-    const currentUser = await dao.findUserById(userId);
-    req.session["currentUser"] = currentUser;
+    // const currentUser = await dao.findUserById(userId);
     res.json(status);
   };
 
